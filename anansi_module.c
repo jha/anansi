5a3a7476cb458174c9192453662e061ab661d806
jha <24598074+jha@users.noreply.github.com>
2018-02-13 19:27:10 -0500

    Print out messages during unloading

--- anansi_module.c
+++ anansi_module.c
@@ -23,11 +23,15 @@ anansi_init(void)
 void __exit
 anansi_exit(void)
 {
+    LOG("unloading...");
+
 #if 1 /* Run on first CPU only */
     smp_call_function_single(0, anansi_transition_exit, NULL, 1);
 #else /* Run on all CPUs */
     on_each_cpu(anansi_transition_exit, NULL, 1);
 #endif
+
+    LOG("Finished!");
 }
 
 module_init(anansi_init);
