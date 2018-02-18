d4acbc2eb705c33d8b322e6d79884bc30a8ae806
jha <24598074+jha@users.noreply.github.com>
2018-02-18 13:28:20 -0500

    Additional VMX instructions

--- anansi_vmx.c
+++ anansi_vmx.c
@@ -42,3 +42,60 @@ anansi_vmx_vmxoff(void)
 
     return TRUE;
 }
+
+bool_t
+anansi_vmx_vmclear(uint64_t vmcs_phaddr)
+{
+    asm volatile (
+        "vmclear %[vmcs]"
+        :
+        : [vmcs] "m" (vmcs_phaddr)
+    );
+
+    /* 31.6 PREPERATION AND LAUNCHING A VIRTUAL MACHINE
+     * Verify succesful execution of VMCLEAR by checking if RFLAGS.CF = 0
+     * and RFLAGS.ZF = 0 */
+    if ((anansi_flags() & X86_FLAG_CF) != 0
+        || (anansi_flags() & X86_FLAG_ZF) != 0)
+    {
+        LOG("VMCLEAR failure");
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+void
+anansi_vmx_vmptrld(uint64_t vmcs_phaddr)
+{
+    asm volatile (
+        "vmptrld %[vmcs]"
+        :
+        : [vmcs] "m" (vmcs_phaddr)
+    );
+}
+
+void
+anansi_vmx_vmwrite(void)
+{
+
+}
+
+bool_t
+anansi_vmx_vmlaunch(void)
+{
+    asm volatile ("vmlaunch");
+
+    /* 31.6 PREPARATION AND LAUNCHING A VIRTUAL MACHINE
+     * If VMLAUNCH fails due to any consistency checks before guest-state
+     * loading, RFLAGS.CF or RFLAGS.ZF will be set and the VM-instruction
+     * error field will contain the error code */
+    if ((anansi_flags() & X86_FLAG_CF) != 0
+        || (anansi_flags() & X86_FLAG_ZF) != 0)
+    {
+        LOG("VMLAUNCH failure");
+        return FALSE;
+    }
+
+    return TRUE;
+}
--- anansi_vmx.h
+++ anansi_vmx.h
@@ -6,7 +6,19 @@
 bool_t
 anansi_vmx_vmxon(uint64_t vmxon_phaddr);
 
-inline void
+bool_t
 anansi_vmx_vmxoff(void);
+
+bool_t
+anansi_vmx_vmclear(uint64_t vmcs_phaddr);
+
+void
+anansi_vmx_vmptrld(uint64_t vmcs_phaddr);
+
+void
+anansi_vmx_vmwrite(void);
+
+bool_t
+anansi_vmx_vmlaunch(void);
 
 #endif /* ANANSI_VMX_H */
